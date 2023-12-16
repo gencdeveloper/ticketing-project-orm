3a92cb7b19e86cd8aac5f41fd29c727bc9b18611
Emre <yunusozudogru34@gmail.com>
2023-12-17 00:03:28 +0100

    Task delete and update porsion completed.

---
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,88 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.entity.Task;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
+import com.cydeo.mapper.TaskMapper;
+import com.cydeo.mapper.UserMapper;
+import com.cydeo.repository.TaskRepository;
+import com.cydeo.service.TaskService;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper) {
+        this.taskRepository = taskRepository;
+        this.taskMapper = taskMapper;
+    }
+
+    private final TaskMapper taskMapper; // Inject the UserRepository
+    @Override
+    public TaskDTO findById(Long id) {
+
+       Optional<Task> task = taskRepository.findById(id);
+       if(task.isPresent()){
+           return taskMapper.convertToDTO(task.get());
+       }
+        return null;
+    }
+
+    @Override
+    public List<TaskDTO> listAllTasks() {
+        //go to database then bring data
+        return taskRepository.findAll().stream().map(taskMapper::convertToDTO).collect(Collectors.toList());
+
+
+    }
+
+    @Override
+    public void save(TaskDTO dto) {
+
+        dto.setTaskStatus(Status.OPEN);
+        dto.setAssignedDate(LocalDate.now());
+        taskRepository.save(taskMapper.convertToEntity(dto));
+
+    }
+
+    @Override
+    public void update(TaskDTO dto) {
+//find current user as a capturing id.I am getting object from database
+        Optional<Task> task = taskRepository.findById(dto.getId());
+
+
+        //converting from dto to entity with map as a updated.
+        Task convertedTask =taskMapper.convertToEntity(dto);
+
+        if(task.isPresent()){
+            convertedTask.setId(task.get().getId());
+            convertedTask.setTaskStatus(task.get().getTaskStatus());
+            convertedTask.setAssignedDate(task.get().getAssignedDate());
+            taskRepository.save(convertedTask);
+        }
+
+    }
+
+    @Override
+    public void delete(Long id) {
+//I will not delete from db
+        //I will change the flag and keep it in the db
+
+        Optional<Task> task = taskRepository.findById(id);
+
+            if(task.isPresent()){
+                task.get().setIsDeleted(true);
+                taskRepository.save(task.get());
+
+            }
+    }
+}
